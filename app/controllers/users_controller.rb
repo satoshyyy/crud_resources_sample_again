91c41f86e0f874fe87eea4c483214711f9554287
“satoshyyy <pancakepancakekuitai@gmail.com>
2022-08-26 21:50:49 +0900

    crudで実装、Unknown actionは未解決

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  # 以下のアクションを追加
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+  # 以下のアクションを追加
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # 以下のアクションを追加
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+
+  # 以下のアクションを追加
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  # 以下のルーティングを定義
+  resources :users
+end
