ca67d2f9596eef13cf0235f2e2d8e7f1d6a47741
“satoshyyy <pancakepancakekuitai@gmail.com>
2022-08-27 02:08:57 +0900

    crudでの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,6 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show
-  end
-
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # 以下のルーティングを定義
-  resources :users
+
+  get "/users", to: "users#index"
+  get "/users/new", to: "users#new"
+  post "/users", to: "users#create"
+  get "/users/:id/edit", to: "users#edit"
+
+  # 以下のルーティングを追加
+  patch "/users/:id", to: "users#update"
+
+  delete "/users/:id", to: "users#destroy"
 end
